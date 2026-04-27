46c0702a04cd720659de34ac663a2da8d8156811
charmingandhi <charmingandhi0910@gmail.com>
2026-04-27 12:07:40 +0530

    fix render path and port

--- Backend/app.py
+++ Backend/app.py
@@ -112,4 +112,7 @@ def predict():
 if __name__ == "__main__":
     app.run(debug=False)   # ❗ important (no auto-restart)
 
-app.run(host='0.0.0.0', port=10000)
+import os
+
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
