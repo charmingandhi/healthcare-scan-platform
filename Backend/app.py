06fae03680ada0b5f15f7bee151425bbab846f89
charmingandhi <charmingandhi0910@gmail.com>
2026-04-27 12:47:21 +0530

    fix keras version compatibility

--- Backend/app.py
+++ Backend/app.py
@@ -3,7 +3,7 @@
 # =======================
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from tensorflow.keras.layers import Dense
 import numpy as np
 from PIL import Image
